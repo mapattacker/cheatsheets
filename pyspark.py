3060a53b14c06f36628d12f55fb89d4ba82300a6
Jake <teo.siyang@gmail.com>
2017-01-27 22:23:54 +0800

    Update pyspark.py

--- pyspark.py
+++ pyspark.py
@@ -10,10 +10,11 @@ sc = SparkContext(conf = conf)
 lines = sc.textFile("file:///Users/Spark/1800.csv")
 parsedLines = lines.map(parseLine)  #use map function, output has same number of entries, just that it can be transformed.
 words = input.flatMap(lambda x: x.split())  #use flat map function, output has more entries than input
-# split data into key/value or just values
 
-parsedLines.countByValue()  #count values, give key & count value
-
+# key/value functions
+  # reduce by key, x & y represent values of same key
+total = parsedLines.reduceByKey(lambda x, y: x + y)
+totalsByAge = rdd.mapValues(lambda x: (x, 1)).reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1]))
 
 # collect the results
 results = words.collect()
