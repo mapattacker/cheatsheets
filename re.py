693b31aba37d4902940269a8b33c9715179e8005
Jake <teo.siyang@gmail.com>
2017-01-29 14:35:09 +0800

    Update re.py

--- re.py
+++ re.py
@@ -7,14 +7,18 @@ import re
   # \d	Matches digits. Equivalent to [0-9].
   # \D	Matches nondigits.
 
+phone = "2004-959-559 # This is Phone Number"
+
+
 # match; output if result at start of string
 # search; output first result in string
+num = re.search(r'2004', phone)
+print num.group()
+
 # findall; output all results in a list
-phone = "2004-959-559 # This is Phone Number"
 num = re.findall(r'2004|Phone', phone)
 print num
 
 # replace
-phone = "2004-959-559 # This is Phone Number"
 num = re.sub(r'\W', '', phone)  #3 arguments
 print num
