0c53c0ce62d4ed671d9d6dd8c7b7d062bdcd6dfa
Jake <teo.siyang@gmail.com>
2017-01-29 13:50:18 +0800

    Update re.py

--- re.py
+++ re.py
@@ -1,1 +1,11 @@
 import re
+
+# https://www.tutorialspoint.com/python/python_reg_expressions.htm
+  # \w	Matches word characters.
+  # \W	Matches nonword characters.
+  # \d	Matches digits. Equivalent to [0-9].
+  # \D	Matches nondigits.
+
+# match
+# search
+# replace
