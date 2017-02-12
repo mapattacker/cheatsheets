dd36c8ec2ee39fbeb248cf2171a05ec2f54dbe22
Jake <teo.siyang@gmail.com>
2017-02-12 13:35:43 +0800

    Update matplot.py

--- matplot.py
+++ matplot.py
@@ -1,1 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+%matplotlib inline
 
+# two main ways to plot
+  #1. when dataframe is outside, you can enter arguements inside OR outside brackets.
+df.plot()
+df.plot(figsize())
+
+  #2 when df is inside, arguments must be entered as a separate line.
+plt.figure(figsize=(18, 5))
+plt.plot(df.index, df['Adj Close'])
