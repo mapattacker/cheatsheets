bc303cbadcb760542bcc77b7c92e13cc13964a42
Jake <teo.siyang@gmail.com>
2017-02-12 13:44:44 +0800

    Update matplot.py

--- matplot.py
+++ matplot.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 # two main ways to plot
   #1. when dataframe is outside, you can enter arguements inside OR outside brackets.
 df.plot()
-df.plot(figsize())
+df.plot(figsize(18,5), legend=True)
 
   #2 when df is inside, arguments must be entered as a separate line.
 plt.figure(figsize=(18, 5))
