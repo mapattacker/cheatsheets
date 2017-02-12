a1772ba234cc0b3bd2dfb64bb68359e1aacb662b
Jake <teo.siyang@gmail.com>
2017-02-12 14:02:52 +0800

    Update matplot.py

--- matplot.py
+++ matplot.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 # two main ways to plot
   #1. when dataframe is outside, you can enter arguements inside OR outside brackets.
 df.plot()
-df.plot(figsize(18,5), legend=True, title='This is a title', label='Adjusted Closing Price')
+df.plot(figsize(18,5), legend=True, title='Frasers Centrepoint Trust', label='Adjusted Closing Price')
 
   #2 when df is inside, arguments must be entered as a separate line.
 plt.figure(figsize=(18, 5))
