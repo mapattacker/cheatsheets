a4b959f4d0e6ec3ab36ae502b1dc0d82e942a014
Jake <teo.siyang@gmail.com>
2017-02-28 22:15:44 +0800

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -39,8 +39,8 @@ df.describe #mean, std, count, etc. only numeric formated columns
 ## FORMAT
 df.dtypes
 df['hour'] = df['hour'].astype('int64')
-df2['time'] = pd.to_datetime(df2['time'])
 df['price'] = pd.to_numeric(df['price'])
+df['Time'] = pd.to_datetime(df['Time'])
 
 
 #--------------------------------------------------------
