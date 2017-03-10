02bc3aaf11e84a364dedb9d1eefc601ce53666c3
Jake <teo.siyang@gmail.com>
2017-03-10 22:09:05 +0800

    Update tableau.sql

--- tableau.sql
+++ tableau.sql
@@ -1,10 +1,12 @@
 --using Tableau parameters to connect to SQL database with (All) option
+	--either define a table with possible and (All) attributes, and define in WHERE clause
 with filter_week AS (select 'Weekend' AS weektype, 'Weekend' AS param
-									  UNION select 'Weekday' AS weektype, 'Weekday' AS param
-									  UNION select 'Weekday' AS weektype, '(All)' AS param
-									  UNION select 'Weekend' AS weektype, '(All)' AS param)
+			  UNION select 'Weekday' AS weektype, 'Weekday' AS param
+			  UNION select 'Weekday' AS weektype, '(All)' AS param
+			  UNION select 'Weekend' AS weektype, '(All)' AS param)
 SELECT *
 FROM DBO.table_nm a
 WHERE WEEKTYPE IN (SELECT weektype FROM filter_week WHERE param=:week)
-      AND (((is_pub_holiday='N' OR is_pub_holiday='Y') AND :p_holiday='(All)') 
-           OR (is_pub_holiday=:p_holiday) OR (is_pub_holiday=:p_holiday))),
+	--or use OR/AND in WHERE clause
+      AND (((holiday='N' OR holiday='Y') AND :holiday='(All)') 
+           OR (holiday=:holiday) OR (holiday=:holiday))),
