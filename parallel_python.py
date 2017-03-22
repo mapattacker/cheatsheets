3a0991715acccc23766a99dbf2c73f2ec3e11aec
Jake Teo <teo.siyang@gmail.com>
2017-03-23 00:20:01 +0800

    Update parallel_python.py

--- parallel_python.py
+++ parallel_python.py
@@ -1,1 +1,14 @@
 
+# https://pythonhosted.org/joblib/parallel.html
+# using 
+
+%%timeit -n 10
+from math import sqrt
+x = [sqrt(i ** 2) for i in range(100000)]
+
+%time
+from joblib import Parallel, delayed
+import multiprocessing
+from math import sqrt
+num_cores = multiprocessing.cpu_count()
+x = Parallel(n_jobs=num_cores)(delayed(sqrt)(i ** 2) for i in range(100000))
