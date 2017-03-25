dffddc9a331e4f71c74b628481b28fc96d00ba8b
Jake Teo <teo.siyang@gmail.com>
2017-03-25 11:43:59 +0800

    Update parallel_python.py

--- parallel_python.py
+++ parallel_python.py
@@ -7,6 +7,34 @@ import multiprocessing
 print multiprocessing.cpu_count()
 
 
+# use multiprocessing pool
+#------------------------------
+from multiprocessing import Pool
+from multiprocessing import cpu_count
+import time
+
+start = time.time()
+core = cpu_count()
+
+def f(x):
+    return x*x
+
+if __name__ == '__main__':
+    test = range(80000000)
+    pool = Pool(processes=core)
+    results = pool.map(f, test)
+
+# test using normal loop
+# record = []
+# for i in range(8000000):
+#     record.append(f(i))
+
+end = time.time()
+print end-start
+#------------------------------
+
+
+
 # https://pythonhosted.org/joblib/parallel.html
 # using 
 
@@ -30,62 +58,3 @@ col = ['a','b','c','d','e','f']
 df = pd.DataFrame(np.random.randint(1, 99, (100000,6)), columns=col)
 
 
-
-#------------------
-%time
-def p1(x):
-    df['g'] = df['a'].apply(lambda x: 999 if x == 1 else x)
-
-def p2(x):
-    df['h'] = df['b'].apply(lambda x: 999 if x == 1 else x)
-
-def p3(x):
-    df['i'] = df['c'].apply(lambda x: 999 if x == 1 else x)
-
-p1(df)
-p2(df)
-p3(df)
-df.head()
-
-
-#------------------
-%time
-from multiprocessing import Pool
-
-def p1(x):
-    df['g'] = df['a'].apply(lambda x: 999 if x == 1 else x)
-def p2(x):
-    df['h'] = df['b'].apply(lambda x: 999 if x == 1 else x)
-def p3(x):
-    df['i'] = df['c'].apply(lambda x: 999 if x == 1 else x)
-    
-pool = Pool()
-result1 = pool.apply_async(p1, [df])    # evaluate "solve1(A)" asynchronously
-result2 = pool.apply_async(p2, [df])    # evaluate "solve2(B)" asynchronously
-result3 = pool.apply_async(p2, [df])    # evaluate "solve2(B)" asynchronously
-answer1 = result1.get()
-answer2 = result2.get()
-
-df.head()
-
-#------------------
-from multiprocessing import Process
-
-def p1(x):
-    df['g'] = df['a'].apply(lambda x: 999 if x == 1 else x)
-def p2(x):
-    df['h'] = df['b'].apply(lambda x: 999 if x == 1 else x)
-def p3(x):
-    df['i'] = df['c'].apply(lambda x: 999 if x == 1 else x)
-    
-p1 = Process(target=p1, args=(df,))
-p1.start()
-p1.join()
-
-p2 = Process(target=p2, args=(df,))
-p2.start()
-p2.join()
-
-p3 = Process(target=p3, args=(df,))
-p3.start()
-p3.join()
