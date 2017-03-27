f7af1ebd71960fd84141cbe83a0c8c4989462daa
Jake Teo <teo.siyang@gmail.com>
2017-03-27 10:43:41 +0800

    Update pc.py

--- pc.py
+++ pc.py
@@ -4,11 +4,21 @@ import glob
 for i in glob.iglob('*eapsim*'):
   print i
   
+
 # get current directory name
 import os
 directoryName = os.path.relpath(".","..")
 
-# get full path, folder name (in a list), and file names (in a list)
+
+# get all files & folders in path listed (as a list)
+import os
+# Open a file
+path = 'C:\Users\Teo Siyang\Dropbox\wifi'
+for i in os.listdir( path ):
+  print i
+
+    
+# get full path, folder name (in a list), and file names (in a list) from path lsited
 import os
 path = 'C:\Users\Teo Siyang\Dropbox\wifi'
 for root, dirs, files in os.walk(path):
