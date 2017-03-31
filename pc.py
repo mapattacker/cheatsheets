4bc69a275c40419ac4a02b560653dbdeb86edddb
Jake Teo <teo.siyang@gmail.com>
2017-03-31 10:26:39 +0800

    Update pc.py

--- pc.py
+++ pc.py
@@ -13,14 +13,14 @@ directoryName = os.path.relpath(".","..")
 # get all files & folders in path listed (as a list)
 import os
 # Open a file
-path = 'C:\Users\Teo Siyang\Dropbox\wifi'
+path = 'C:\Users\Teo Siyang\'
 for i in os.listdir( path ):
   print i
 
     
 # get full path, folder name (in a list), and file names (in a list) from path lsited
 import os
-path = 'C:\Users\Teo Siyang\Dropbox\wifi'
+path = 'C:\Users\Teo Siyang\'
 for root, dirs, files in os.walk(path):
     print root, dirs, files
 
