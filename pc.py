d4f3e677e7fe78d5671324852d8b2836d02a9631
Jake Teo <teo.siyang@gmail.com>
2017-03-31 09:56:03 +0800

    Update pc.py

--- pc.py
+++ pc.py
@@ -25,7 +25,7 @@ for root, dirs, files in os.walk(path):
     print root, dirs, files
 
     
-    
+# http://en.cppreference.com/w/cpp/io/c/fopen
 # File access mode string	| Meaning	| Explanation	Action if file already exists | Action if file does not exist
 # "r"	read | Open a file for reading	| read from start	| failure to open
 # "w"	write	| Create a file for writing	| destroy contents | create new
