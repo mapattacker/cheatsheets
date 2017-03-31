b5bffedc9a7a61c09c9ed73b916d32594ca5cff4
Jake Teo <teo.siyang@gmail.com>
2017-03-31 09:57:34 +0800

    Update pc.py

--- pc.py
+++ pc.py
@@ -26,10 +26,10 @@ for root, dirs, files in os.walk(path):
 
     
 # http://en.cppreference.com/w/cpp/io/c/fopen
-# File access mode string | Meaning | Explanation	Action if file already exists | Action if file does not exist
-# "r"	read | Open a file for reading	| read from start	| failure to open
-# "w"	write	| Create a file for writing	| destroy contents | create new
+# File access mode string | Meaning | Explanation | Action if file already exists | Action if file does not exist
+# "r"	read | Open a file for reading | read from start | failure to open
+# "w"	write | Create a file for writing | destroy contents | create new
 # "a"	append | Append to a file | write to end | create new
-# "r+"	read extended	| Open a file for read/write	read from start	error
-# "w+"	write extended	| Create a file for read/write | destroy contents	| create new
-# "a+"	append extended	| Open a file for read/write | write to end	| create new
+# "r+"	read extended | Open a file for read/write | read from start error
+# "w+"	write extended | Create a file for read/write | destroy contents | create new
+# "a+"	append extended | Open a file for read/write | write to end | create new
