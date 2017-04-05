b3856e544c8ad6cb11d70ef96c8444f5e2e58db3
Jake Teo <teo.siyang@gmail.com>
2017-04-05 22:15:51 +0800

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -68,7 +68,7 @@ df['Time'] = pd.to_datetime(df['Time'], errors='coerce')
 
 #--------------------------------------------------------
 ## Using SQL
-import psycopg2
+import psycopg2 #postgres connection
 conn = psycopg2.connect(database="postgres", user="postgres", password="***", host="127.0.0.1", port="5432")
 query = ''' SELECT *
             FROM customer
