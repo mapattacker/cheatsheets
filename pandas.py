94b1f3fbfcedcf79d53e8e12bc25bfa4c72a8da5
Jake Teo <teo.siyang@gmail.com>
2017-04-22 11:34:00 +0800

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -74,6 +74,7 @@ df['Time'] = pd.to_datetime(df['Time'], errors='coerce')
 import psycopg2 
 conn = psycopg2.connect(database="postgres", user="postgres", password="***", host="127.0.0.1", port="5432")
     # OR use sqlalchemy, which supports most databases
+    # database engine + database connector package://username:password @ host ip / database? encoding
 from sqlalchemy import create_engine
 conn = create_engine('postgresql://postgres:password@localhost:5432/postgres')
 
