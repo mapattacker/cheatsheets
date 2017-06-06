aff88394859822595111dd6032d59705e00041c3
Jake Teo <teo.siyang@gmail.com>
2017-06-06 13:33:02 +0800

    Update and rename nodejs.txt to node.js

---
+++ node.js
@@ -0,0 +1,18 @@
+--------------BASH
+// check node.js version
+node -v
+
+// check node package manager version
+npm -v
+
+// list all node packages
+npm -g list
+npm ls
+
+// check node package
+node -g list package_name
+
+---------------------------
+// print
+var foo = {bar: 'foobar'};
+console.log(foo);
