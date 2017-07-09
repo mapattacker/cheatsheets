612ce0ba2457f799667c82fca7cde0fe86380c94
Jake Teo <siyang@SCMC-LT10s-MBP.local>
2017-07-09 11:35:06 +0800

    .

--- d3.js
+++ d3.js
@@ -5,23 +5,25 @@
 <script src="https://d3js.org/d3-selection.v1.min.js"></script>
 
 
-// SELECTION
+//////////////////// SELECTION ////////////////////
   // select will grab the first selection it encounters
   // selectAll will grab all selection
 d3.select(".main-title") //select class
 d3.select("#header") // select id
 d3.select("h1") // select tag
+d3.select("img").attr("src") // select an attribute eg., <img src="./assets/udacity.svg">
 
   // nested selection
-d3.select("#header img") // select parent id=header, and then 
+d3.select("#header img") // select parent id=header, and then followed by the child tag <img>. Need not be direct child
 
 
-// SWAP
+
+//////////////////// SWAP ////////////////////
   // change text
 d3.select(".main-title").text("I changed the title");
 
 
-// TRANSITIONS
+//////////////////// TRANSITIONS ////////////////////
 
 // fade transition
 d3.select("body").transition()
