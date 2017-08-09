da01932c6f8779073956c7b104058e8a8e0dc56a
Jake Teo <siyang@SCMC-LT10s-MBP.local>
2017-08-10 00:29:01 +0800

    .

---
+++ python_basics.py
@@ -0,0 +1,6 @@
+# STRING FORMAT
+# more: https://mkaz.tech/code/python-string-format-cookbook/
+print 'this score is {}'.format(1.1234)
+>>> 'this score is 1.1234'
+print 'this score is {:.2f}'.format(1.1234) # 2 decimal format
+>>> 'this score is 1.12'
