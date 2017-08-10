544d5e373b04f98d5809cea29356e443976e7a26
Jake Teo <siyang@SCMC-LT10s-MBP.local>
2017-08-10 16:02:17 +0800

    .

--- python_basics.py
+++ python_basics.py
@@ -4,3 +4,11 @@ print 'this score is {}'.format(1.1234)
 >>> 'this score is 1.1234'
 print 'this score is {:.2f}'.format(1.1234) # 2 decimal format
 >>> 'this score is 1.12'
+
+
+# DIVISION IN DECIMALS
+# in python, there is no decimals if you do a division. Use the following to obtain it.
+>>> 4 / float(100)
+0.04
+>>> 4 / 100.0
+0.04
