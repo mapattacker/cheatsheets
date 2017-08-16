7c0da0407f7d0d0f417c0b23c37dc71f20610ee5
Jake Teo <siyang@SCMC-LT10s-MBP.local>
2017-08-16 22:55:04 +0800

    .

--- sql.sql
+++ sql.sql
@@ -1,4 +1,5 @@
-  
+--SCHEMATICS
+--------------------------------------
 --Alter Existing Table
 ALTER TABLE tablename RENAME fieldname1 to fieldname2
 ALTER TABLE table_name ADD fieldname float;
@@ -17,16 +18,21 @@ CREATE table public.tablename (
 ALTER TABLE tablename ADD PRIMARY KEY (fieldname);
 CREATE INDEX indexname ON tablename (fieldname);
 
+--insert
+insert into operator_lookup(user_nm_domain, operator) values ('y5zwew.sg','testdata');
+
+--delete all data from table
+delete from ts_biz_100m_ap;
+
 --QUERIES
 --------------------------------------
-
 --select consecutive before & after rows
 select *,
       lead(column_nm) over(partition by column_nm2 order by column_nm3), --selects the row below the current
       lead(column_nm) over(partition by column_nm2 order by column_nm3), --selects the row above the current
 from table_name 
 
---conditional selection
-
-(CASE when lag(ap_name) over(partition by user_mac, start_date order by start_timestamp) = ap_name then 1 else 0 end)
+--if else
+select (case when column1 - column2 >= 14 then 1 else 0) as difference
+from table_name
 
