18770fbe4de5cc0edc1a881dae759ee52c28a0dd
Jake Teo <siyang@SCMC-LT10s-MBP.local>
2017-08-17 23:47:31 +0800

    .

--- python_basics.py
+++ python_basics.py
@@ -1,4 +1,5 @@
 # STRING FORMAT
+#--------------------------------
 # more: https://mkaz.tech/code/python-string-format-cookbook/
 print 'this score is {}'.format(1.1234)
 >>> 'this score is 1.1234'
@@ -7,8 +8,22 @@ print 'this score is {:.2f}'.format(1.1234) # 2 decimal format
 
 
 # DIVISION IN DECIMALS
+#--------------------------------
 # in python, there is no decimals if you do a division. Use the following to obtain it.
 >>> 4 / float(100)
 0.04
 >>> 4 / 100.0
 0.04
+
+
+# DATE
+#--------------------------------
+from datetime import datetime
+
+x = datetime.strptime('2011-01-03', '%Y-%m-%d')
+print x
+# 2011-01-03 00:00:00
+
+x = datetime.strptime('2011-01-03', '%Y-%m-%d').date()
+print x
+# 2011-01-03
