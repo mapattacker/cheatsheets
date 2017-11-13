335994733661502daeabb8d9368f0e1613ed5369
Jake Teo <xxx>
2017-11-13 09:13:50 +0800

    .

--- web development/php_basics.php
+++ web development/php_basics.php
@@ -14,15 +14,9 @@
 
     // SQL query for total species count
     $speciescount = "select count(*) from species_info";
-    $runresult1 = mysqli_query($conn, $speciescount);
-    $speciescount = mysqli_fetch_array($runresult1);
+    $runresult1 = mysqli_query($conn, $speciescount); // retrieve results from DB
+    $speciescount = mysqli_fetch_array($runresult1); // put results in arrary
     $speciescount = $speciescount[0];
-
-    //query for total family count
-    $familycount = "select count(distinct family_id) from species_info";
-    $runresult2 = mysqli_query($conn, $familycount);
-    $familycount = mysqli_fetch_array($runresult2);
-    $familycount = $familycount[0];
 
     //sql query to get top 9 species by date
     $query = "select concat(genus, ' ', species) as speciesnm, url_imgthumb, url
@@ -72,28 +66,31 @@
   echo "$test";
 ?>
 
-<!-- ESCAPE CHARACTERS -->
-
+<!-- ESCAPE CHARACTERS ---------------------------------------------->
 <!-- for normal single escape, use \ -->
 <?php <a href="../$url_family[$i]">$family[$i]</a> ?>
 
 <!-- to escape all quotes in echo, use "<<<" with a variable name. 
       note that closing variable can only have ";" with no spaces -->
 <?php echo <<< heredoc
-     <strong>Family</strong>: <a href="../$url_family[$i]">$family[$i]</a>
+    <p>
+      <strong>Family</strong>: <a href="../$url_family[$i]">$family[$i]</a>
+    </p>
 heredoc;
 ?>
 
 
 
 <?php
-  // TYPE
-  // ----------------------
-  echo gettype(1)
+  // PRINTING
+  // --------------------------------------------
+  $test = "testing"
+  print_r($test); // print actual value / array
+  var_dump($test); // like print_r but with type & # char. Also prints False
   
 
   // CONCAT
-  // ----------------------
+  // --------------------------------------------
   // concat using .
   $x = "this" . " " . "is great";
   echo "$x";
@@ -105,7 +102,7 @@ heredoc;
   
   
   // CONDITION
-  // ----------------------
+  // --------------------------------------------
   // ternary ?:
   $www = 123;
   $msg = ($www > 100) ? "Large" : "Small"
@@ -128,7 +125,7 @@ heredoc;
   echo strpos($test, "es"); // careful using strpos as a condition, use === to evaluate as False is 0 & position 1 is also 0
   
   // LOOPS
-  // ----------------------
+  // --------------------------------------------
   // while loop
   $fuel = 10;
   while ($fuel < 1) {
@@ -136,15 +133,81 @@ heredoc;
     $fuel = $fuel - 1
   }
   
-  // for loops
+  // for loops (linear array)
+  for ($count=0; $count<count($array); $count++) { //start, end if, after each loop
+    echo "$i, $stuff[$i]\n";
+  }
   
+  // for each loop (array looping)
+  foreach($stuff as $key => $value){
+    echo "$key: $value\n";
+  }
   
   
   // CAST
-  // ----------------------
+  // --------------------------------------------
   echo (int)9.9 + 1
   echo "sam" . 25 // . will force convert 25 into string
   echo "25" + 25 // + will force convert "25" into number
 ?>
 
-
+<?php
+  // ARRAY 
+  // --------------------------------------------
+  $stuff = array("hi", "there");
+  echo $stuff[0]; // hi
+  
+  $stuff = array("name" => "Jake",
+                 "country" => "Asia");
+  echo $stuff["name"];
+  
+  // print exact array with preserved indent
+  echo "<pre>";
+  print_r($stuff);
+  echo "</pre>";
+  
+  // add stuff to array, must be new field
+  $stuff["name2"] = "Emila";
+  
+  // NESTED ARRAYS
+  $products = array(
+              "paper" => array(
+                  "copier" => "Multipurpose",
+                  "inkjet" => "Inkjet Printer"
+                  )
+              );
+  echo $products["paper"]["copier"]; // Multipurpose
+  
+  // check for key availability 
+  echo $stuff["name"] ?? 'nobody'; // if yes print name, else print "nobody" (from PHP 7)
+  echo isset($stuff["name"]) ? $stuff["name"]: "nobody"; 
+  
+  // SORTING, function will change the array, print again to see the change
+  sort($array) // sorts array values (loses keys)
+  ksort($array) // sorts array by keys
+  asort($array) // sorts array by values (keeping keys)
+  shuffle($array) // sorts randomly
+  print_r($array)
+  
+  // SORT
+  // Array
+  // (
+  //     [0] => Asia
+  //     [1] => Emila
+  //     [2] => Jake
+  // )
+  
+  
+  // STRING TO ARRAY
+  $sentence = "this is a sentence";
+  $explode = explode(' ', $sentence);
+  print_r($explode);
+  
+  // Array
+  // (
+  //     [0] => this
+  //     [1] => is
+  //     [2] => a
+  //     [3] => sentence
+  // )
+?>
