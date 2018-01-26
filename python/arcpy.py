aa49e58296ae32e598b779daaa9aff9cd2dba6e1
Jake Teo <xxx>
2018-01-26 10:45:29 +0800

    added in memory tricks

--- python/arcpy.py
+++ python/arcpy.py
@@ -1,18 +1,21 @@
 import arcpy
 import os
 
-# set folder/geodatabase, etc, where your spatial files are stored
+# -----------------------
+# set folder/geodatabase, etc, where your spatial files are stored 
 # not necessary but convenient to prevent typing the path out
 arcpy.env.workspace = "C:\Users\jake\Documents\ArcGIS\\nearest.gdb\\near_gr"
 
 
-# list all feature classes in database
+
+# list all feature classes in database -----------------------
 # also list shapefiles
 for i in arcpy.ListFeatureClasses():
     print i
 
 
-# get unique values from a field
+
+# get unique values from a field -----------------------
 def unique(table, field):
     with arcpy.da.SearchCursor(table, [field]) as cursor:
         listset = set([row[0] for row in cursor]) # remove duplicates
@@ -21,3 +24,19 @@ def unique(table, field):
 table = "C:\Users\siyang\Documents\ArcGIS\singapore.gdb\gr\el"
 field = 'ED_EC'
 print unique(table, field)
+
+
+
+# use definition query -----------------------
+# function only exist in layer, so have to convert that first
+arcpy.MakeFeatureLayer_management(os.path.join(workspace,"pa_main"), 
+                                        "pa_main_lyr", 
+                                        where_clause=where)
+
+
+
+# making output as temporary layers/files -----------------------
+# just add "in_memory/layer_name" as the output
+arcpy.Buffer_analysis("Roads", "in_memory/Buffers", 1000)
+# delete in memory dataset
+arcpy.Delete_management("in_memory/join1")
